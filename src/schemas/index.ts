a1fc9fb08d7b74c2a4c225d0328dedaed1906797
Samuel <samuel@thegreg.ai>
2025-06-13 21:21:37 +0000

    ch: auth zod schema

---
+++ src/schemas/index.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+const signupSchema = z.object({
+  firstName: z.string().min(1, { message: "First name is required" }).max(50, { message: "First name must be at most 50 characters long" }),
+  lastName: z.string().min(1, { message: "Last name is required" }).max(50, { message: "Last name must be at most 50 characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(8, { message: "Password must be at least 8 characters long" })
+    .max(100)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+      message: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    }),
+  confirmPassword: z.string().min(8).max(100)
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"],
+});
+
+const signinSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(8, { message: "Password must be at least 8 characters long" })
+})
+
+export {signinSchema, signupSchema};
